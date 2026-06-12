5f03cabc03714ba493beaf9312069f8ef7b3d176
MarinaDP2006 <marinadompas@gmail.com>
2026-06-12 12:19:56 +0200

    Create editions-page.component.ts

---
+++ src/app/pages/editions/editions-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editions-page',
+  template: `
+    <div class="editions-page">
+      <h1>Ediciones Especiales</h1>
+      <p class="coming-soon">Próximamente más información sobre ediciones especiales, reediciones y versiones alternativas.</p>
+    </div>
+  `,
+  styleUrls: ['./editions-page.component.scss']
+})
+export class EditionsPageComponent {}
